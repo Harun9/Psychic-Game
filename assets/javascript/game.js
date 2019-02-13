6105e7b064c1034eca3c37d2a3badc668258fe6d
Harun9 <harunrush9@gmail.com>
2019-02-12 19:04:37 -0600

    progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,87 +1,32 @@
-// Creating a function to determine if user correctly guessed
-function pyschic(user, computer) {
-    if (user === computer) {
-        return "Win";
-    } else {
-        return "Incorrect";
-    }
-}
-
-// Checking whether user input is in the userChoices array
-function inArray(user, arr) {
-    var count = arr.length;
-    for (var i = 0; i < count; i++) {
-        if (arr[i] === user) {
-            return true;
-        }
-    }
-    return false;
-}
-
-// Reset the funtion after a win/loss
-function reset() {
-    guessesLeft = 10;
-    guessedLetters = [];
-    computerLetter =
-        computerChoices[Math.floor(Math.random() * computerChoices.length)];
-}
-
-// Creates an array for the computer choices (entire alphabet - lowercase)
-computerChoices = [];
-for (i = 100; i <= 122; i++) {
-    computerChoices[computerChoices.length] = String.fromCharCode(i);
-}
-
-// Connect variables to HTML & starting position
+var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
+var remainingGuesses = 10;
 var guessedLetters = [];
+var userGuess = document.onkeyup;
+var compGuess = letters[Math.floor(Math.random() * letters.length)];
 
-// Get computer's guess
-var computerLetter =
-    computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-// This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
-    // Checking what the computer generated for testing purposes
-    console.log(computerLetter);
 
-    // Determines which key was pressed.
-    var userGuess = event.key;
-    console.log(userGuess);
+    userGuess = event.key;
+    guessedLetters.push(userGuess);
 
-    // Checks if input is accpetable
-    var validLetter = inArray(userGuess, computerChoices);
-    console.log(validLetter);
-
-    // creating a "result" variable for the resilt of the psychic function
-    var result = pyschic(userGuess, computerLetter);
-    console.log(result);
-
-    // Creating an "If" stmt to first check if valids input, and then if win/incorrect result
-    if (validLetter == true) {
-        // Creating an "If" stmt to tell what to do for a win/incorrect result
-        if (result == "Win") {
-            // add a win into the log
-            wins++;
-            reset();
-        } else {
-            // reduce the guesses left
-            guessesLeft--;
-            // record the guesses so far
-            guessedLetters.push(userGuess);
-        }
-
-        if (guessesLeft == 0) {
-            // add a loss into the log
-            losses++;
-            reset();
-        }
-    } else {
-        alert("Invalid input! Please use the alphabet :)");
+    if (compGuess == userGuess) {
+        wins++;
+        remainingGuesses = 10;
+        guessedLetters = [];
     }
 
+    if (compGuess != userGuess) {
+        remainingGuesses--;
+
+    }
+
+    if (remainingGuesses == 0) {
+        losses++;
+        remainingGuesses = 10;
+        guessedLetters = [];
+    }
     // Changing HTML text
     document.getElementById("wins").innerHTML = wins;
     document.getElementById("losses").innerHTML = losses;
